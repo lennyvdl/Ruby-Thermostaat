4e96a38547362e887bd59f7f89d9034ae71f37bd
lenny vanderlinden <lenny vanderlinden>
2017-03-14 12:04:56 +0100

    Made it possible to give in values in cli

--- app.rb
+++ app.rb
@@ -13,7 +13,16 @@ puts foo.name
 =end
 
 foo = Temperature.new
+foo.clitemp(ARGV[0].to_i,ARGV[1].to_i)
+=begin
+foo.ask_unit
+foo.get_unit
+
+
 foo.ask_temp
 foo.get_temp
+
 foo.range
-foo.colour_led
+
+foo.convert
+=end
--- temperature.rb
+++ temperature.rb
@@ -1,25 +1,68 @@
 class Temperature
-  attr_accessor :range
-  attr_accessor :temp
+=begin
+  def ask_unit
+    puts "What unit are you using????? k = kelvin, c = celsius, f = Fahrenheit aka pleb unit"
+  end
+
   def ask_temp
     puts "Whats the temperature? "
   end
 
+
   def get_temp
-    @temperature = gets.to_i
+    temperature = ARGV[0].to_i
   end
 
   def range
-    @range = 1
+    range = ARGV[1].to_i
   end
 
-  def colour_led
-    if @temperature > (20 +@range)
-      puts "red"
-    elsif @temperature < (20 -@range)
-      puts "blue"
-    else
-      puts "green"
+  def get_unit
+    unit = ARGV[2].gets.downcase.delete!("\n")
+  end
+=end
+
+  def convert
+    if @unit == "f"
+    @endtemp = (@temperature* 1.8 + 32)
+    @endrange = (@range*1.8 + 32)
+      puts "Fahrenheit selected and temperature is #{@endtemp}."
+    elsif @unit == "k"
+      @endtemp = (@temperature+273.15)
+      @endrange = (@range+273.15)
+      puts "Kelvin selected"
+    elsif @unit =="c"
+      puts "Celcius selected"
     end
   end
+
+  def clitemp(temperature,range)
+    @temperature= temperature
+    @range = range
+    #@unit = unit
+
+        if @temperature > (20 +@range)
+          puts "red"
+        elsif @temperature < (20 -@range)
+          puts "blue"
+        else
+          puts "green"
+        end
+    end
+
+=begin
+  def colour_led(temperature,range)
+    @temperature= temperature
+    @range = range
+    #@unit = unit
+
+        if @endtemp > (20 +@endrange)
+          puts "red"
+        elsif @endtemp < (20 -@endrange)
+          puts "blue"
+        else
+          puts "green"
+        end
+    end
+=end
 end
