d3bb4d5c4f0950f4967bd15464bdfbba444ed676
lennyvanderlinden <lenny.vanderlinden@student.vives.be>
2017-05-02 10:35:44 +0200

    added url reader

--- app.rb
+++ app.rb
@@ -1,28 +1,9 @@
 require_relative "temperature"
 
-# require_relative "hello_world"
-
-=begin
-foo = Helloworld.new "Lenny"
-foo.say_hello
-
-foo.name = "Joske"
-foo.say_hello
-
-puts foo.name
-=end
+require 'openssl'
+OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE
+require "open-uri"
 
 foo = Temperature.new
-foo.clitemp(ARGV[0].to_i,ARGV[1].to_i)
-=begin
-foo.ask_unit
-foo.get_unit
-
-
-foo.ask_temp
-foo.get_temp
-
-foo.range
-
-foo.convert
-=end
+foo.clitemp(ARGV[0].to_i,ARGV[1].to_i,ARGV[2])
+foo.open
