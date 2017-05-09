5c6df27906b0cd2cfa9ea3f304469b7d6af8d760
lennyvanderlinden <lenny.vanderlinden@student.vives.be>
2017-05-09 10:26:09 +0200

    Refactoring and working on single line responsibility

--- app.rb
+++ app.rb
@@ -1,5 +1,7 @@
 require_relative "temperature"
 
 
+
 foo = Temperature.new
+
 foo.clitemp(ARGV[0].to_i,ARGV[1].to_i,ARGV[2])
--- temperature.rb
+++ temperature.rb
@@ -1,27 +1,15 @@
+require_relative "convert"
 class Temperature
-  def convert
-    if @unit == "f"
-    @endtemp = (@temperature* 1.8 + 32)
-    @endrange = (@range*1.8 + 32)
-      puts "Fahrenheit selected and temperature is #{@endtemp}."
-    elsif @unit == "k"
-      @endtemp = (@temperature+273.15)
-      @endrange = (@range+273.15)
-      puts "Kelvin selected"
-    elsif @unit =="c"
-      puts "Celcius selected"
-    end
-  end
-
   def clitemp(temperature,range,unit)
-    @temperature= temperature
-    @range = range
-    @unit = unit
-    convert
+    $temperature= temperature
+    $range = range
+    $unit = unit
+    foo = Convert.new
+    foo.convert_temp
 
-        if @temperature > (20 +@range)
+        if $temperature > (20 +$range)
           puts "red"
-        elsif @temperature < (20 -@range)
+        elsif $temperature < (20 -$range)
           puts "blue"
         else
           puts "green"
