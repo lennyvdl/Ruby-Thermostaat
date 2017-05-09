73623f6af5fc48d0d84b3d7d718534116b3ff944
lennyvanderlinden <lenny.vanderlinden@student.vives.be>
2017-05-09 09:57:57 +0200

    Finished url reader class

--- url_reader.rb
+++ url_reader.rb
@@ -1,13 +1,11 @@
 class url_reader
-
   require 'openssl'
   OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE
   require "open-uri"
 
   def open
-    value = (URI.parse("https://labict.be/software-engineering/temperature/api/temperature/fake").read).to_f
-    puts value
-
+    url = (URI.parse("https://labict.be/software-engineering/temperature/api/temperature/fake").read).to_f
+    puts url
   end
 
 
