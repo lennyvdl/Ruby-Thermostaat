3573edc3dbe28d7c7bd2e05aed52fa211dc2c84f
lennyvanderlinden <lenny.vanderlinden@student.vives.be>
2017-05-09 09:58:50 +0200

    added cli

--- app.rb
+++ app.rb
@@ -1,9 +1,5 @@
 require_relative "temperature"
 
-require 'openssl'
-OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE
-require "open-uri"
 
 foo = Temperature.new
 foo.clitemp(ARGV[0].to_i,ARGV[1].to_i,ARGV[2])
-foo.open
