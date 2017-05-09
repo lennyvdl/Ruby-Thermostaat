8847290211024223c340f1fe35841f6bb63304c3
lennyvanderlinden <lenny.vanderlinden@student.vives.be>
2017-05-09 11:08:18 +0200

    integrating option parser

--- app.rb
+++ app.rb
@@ -1,7 +1,25 @@
 require_relative "temperature"
-
-
+require 'optparse'
 
 foo = Temperature.new
 
 foo.clitemp(ARGV[0].to_i,ARGV[1].to_i,ARGV[2])
+
+
+options = {:url => nil, :temp => nil}
+
+parser = OptionParser.new do|opts|
+	opts.banner = "Usage: years.rb [options]"
+	opts.on('-u', '--url url', 'Url') do |Url|
+		options[:url] = name;
+	end
+
+	opts.on('-a', '--age age', 'Age') do |age|
+		options[:age] = age;
+	end
+
+	opts.on('-h', '--help', 'Displays Help') do
+		puts opts
+		exit
+	end
+end
